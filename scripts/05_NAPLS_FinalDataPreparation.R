d57871499ff4b88e29332b645d0ff28f25c33ec1
Lukas Roell <71489783+LuRoe7@users.noreply.github.com>
2026-06-23 13:06:48 +1000

    Update 05_NAPLS_FinalDataPreparation.R

--- scripts/05_NAPLS_FinalDataPreparation.R
+++ scripts/05_NAPLS_FinalDataPreparation.R
@@ -15,9 +15,9 @@ p_load(tidyr,dplyr,stringr,readxl,ggplot2,gridExtra,data.table)
 # show working directory and ensure that the folder results including the data is located there
 getwd()
 # define input directory for behavioral and functional connectivity data
-in_dir = "results/napls/04_NAPLS_BehavData_PrepareMedication/"
+in_dir = "PATH/TO/DIRECTORY/"
 # define output directory
-out_dir = "results/napls/05_NAPLS_FinalDataPreparation/"
+out_dir = "PATH/TO/DIRECTORY/"
 # create output directory for this script
 dir.create(out_dir, recursive = TRUE,showWarnings=F)
 # read behavioral data
