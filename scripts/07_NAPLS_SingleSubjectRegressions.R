9ec74c41305a5bec4392aa3d463c8ca053b71891
Lukas Roell <71489783+LuRoe7@users.noreply.github.com>
2026-09-01 10:44:20 +1000

    Change directory paths in NAPLS script
    Updated input and output directory paths for the script.

--- scripts/07_NAPLS_SingleSubjectRegressions.R
+++ scripts/07_NAPLS_SingleSubjectRegressions.R
@@ -15,9 +15,9 @@ p_load(readr,tidyr,dplyr,stringr,ggplot2,ggrepel,gridExtra,ggExtra,readxl,writex
 # show working directory and ensure that the folder results including the data is located there
 getwd()
 # define input directory for behavioral and FC data
-in_dir = "results/napls/05_NAPLS_FinalDataPreparation/"
+in_dir = "PATH/TO/DIRECTORY/"
 # define output directory
-out_dir = "results/napls/07_NAPLS_SingleSubjectRegressions/"
+out_dir = "PATH/TO/DIRECTORY/"
 # create output directory for this script
 dir.create(out_dir, recursive = TRUE,showWarnings=F)
 # read merged data including clinical and behavioral variables
